a55c35731ce0e3c2d211205a611132bd494add64
Jay <rright815@gmail.com>
2023-06-16 16:38:18 +0900

    Styling_ShowingIMG

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,19 @@
+import Image from 'next/image'
 import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
-      
-      <div>©2023 JayJayJay. All rights reserved.</div>
-      <div></div>
-    </div>
+      <div className={styles.container}>
+        <div>©2023 JayJayJay. All rights reserved.</div>
+      <div className={styles.social}>
+
+        <Image src="/1.png"  width={15} height={15} className={styles.icon} alt="Jay Dev Facebook Account"/>
+        <Image src="/2.png" width={15} height={15} className={styles.icon} alt="Jay Dev"/>
+        <Image src="/3.png"  width={15} height={15} className={styles.icon} alt="Jay Dev"/>
+        <Image src="/4.png"  width={15} height={15} className={styles.icon} alt="Jay Dev"/>
+        </div>
+      </div>
   )
 }
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from 'next/link'
 import React from 'react'
+import styles from './page.module.css'
 
 const links = [
     {
@@ -37,14 +38,16 @@ const links = [
 
 const Navbar = () => {
   return (
-    <div>
-        <Link href="/">JayJay</Link>
-        <div>
+    <div className={styles.container}>
+        <Link href="/" className={styles.logo}>JayJay</Link>
+        <div className={styles.links}>
           {links.map((link)=>(
-            <Link key={link.id} href={link.url}> 
+            <Link key={link.id} href={link.url} className={styles.link}> 
             {link.title}</Link>
           ))}
-          <button onClick={()=>{
+          <button 
+          className={styles.logout}
+           onClick={()=>{
             console.log("logged out");
           }}>logout</button>
         </div>
