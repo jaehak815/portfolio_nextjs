18f55040c6bd59aea42347232b34c5e550c9f0dd
Jay <rright815@gmail.com>
2023-06-23 17:21:11 +0900

    SWR_mutationForDashboard

--- src/app/api/posts/[id]/route.js
+++ src/app/api/posts/[id]/route.js
@@ -19,3 +19,17 @@ const {id} = params
 }
 
 }
+
+export const DELETE = async (request, { params }) => {
+    const { id } = params;
+  
+    try {
+      await connect();
+  
+      await Post.findByIdAndDelete(id);
+  
+      return new NextResponse("Post has been deleted", { status: 200 });
+    } catch (err) {
+      return new NextResponse("Database Error", { status: 500 });
+    }
+  };
